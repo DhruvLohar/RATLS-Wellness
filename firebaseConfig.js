46144438e76852608dbd762d7bf6fe8e034bd3c4
DhruvLohar <dhruvlohar09@gmail.com>
2024-07-17 23:22:37 +0530

    added firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+// import { initializeApp } from 'firebase/app';
+
+// // Optionally import the services that you want to use
+// // import {...} from "firebase/auth";
+// // import {...} from "firebase/database";
+// // import {...} from "firebase/firestore";
+// // import {...} from "firebase/functions";
+// // import {...} from "firebase/storage";
+
+// // Initialize Firebase
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDLlf9ZUABsMeHZ6rpSB6mBfXfmon9eySU",
+//   authDomain: "ratlswellness-3a150.firebaseapp.com",
+//   projectId: "ratlswellness-3a150",
+//   storageBucket: "ratlswellness-3a150.appspot.com",
+//   messagingSenderId: "261729220393",
+//   appId: "1:261729220393:web:e8a795863febbb05172938"
+// };
+
+// export default app = initializeApp(firebaseConfig);
+// // For more information on how to access Firebase in your project,
+// // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
